0a00ab71524093cd6774e4fde348dc530be68b33
IYERM <michaeliyer@gmail.com>
2025-04-19 11:04:40 -0400

    Franky Business added with songs

--- config_3.js
+++ config_3.js
@@ -9,7 +9,15 @@ const audioSources3 = {
   },
   audio1: {
     url: "https://dl.dropboxusercontent.com/scl/fi/1noephv3loufu5iiyvh6m/massenetM-ditation.mp3?rlkey=x9kzyu5ohlh1bbffmuebvflhm&st=25p4se3c&dl=0",
-    title: "1. Somebody That I Used To Know",
+    title: "1. Christian Li Meditation",
+  },
+  audio2: {
+    url: "https://dl.dropboxusercontent.com/scl/fi/e8zxvhxreppcitfhokznz/Wheels-on-the-Bus-CocoMelo.mp3?rlkey=pr9m1a5g1nwibgfxcgyd9tfe3&st=db7fpgls&dl=0",
+    title: "1. CocoMelon: The Wheels On the Bus",
+  },
+  audio3: {
+    url: "https://dl.dropboxusercontent.com/scl/fi/wnpm3vswe6wjvcp1p8vbh/theWiggles_HeadShouldersKneesToes.mp3?rlkey=o6y1hakentgh4qmm1z5egrjne&st=hxsjbqx6&dl=0",
+    title: "3. The Wiggles: Head, Shoulders, Knees and Toes",
   },
 
   // Add more tracks here as needed
